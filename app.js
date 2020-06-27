5213306a7df7ee37a46ea61274fcc2817ff6cea0
Asifali85360 <64397536+Asifali85360@users.noreply.github.com>
2020-06-27 15:46:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
+    
+}
+function clearResult(){
+    var result=document.getElementById("Result");
+    result.value="0";
+}
